1fb6eb53f41218c260b6df4ec27d89c0d332a68b
Michael <sanchezmichael241@gmail.com>
2022-04-07 11:06:02 -0400

    Added test cases to Hangman

--- HangmanProject/src/test/java/MainTests.java
+++ HangmanProject/src/test/java/MainTests.java
@@ -1,2 +1,56 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import static org.junit.Assert.*;
+
 public class MainTests {
+    @Before
+    public void StartTest() {
+        Main.resetScore();
+        Main.word = "assign";
+        Collections.addAll(Main.wordArray, Main.word.split(""));
+    }
+
+    @Test
+    public void guessingCorrectWordShouldReturnMessage() {
+        assertEquals(200, Main.letterGuess(Main.word));
+        assertTrue(Main.interpretGuess(200, Main.word));
+    }
+
+
+    @Test
+    public void guessingTheSameLetterShouldReturnAlreadyGuessed() {
+        Main.lettersGuessed.add("b".charAt(0));
+        assertEquals(Main.letterGuess("b"), 401);
+
+    }
+    @Test
+    public void guessingCorrectLetterShouldReplaceDash() {
+        for (String ignored : Main.wordArray){
+            Main.displayedWord.add("_");
+        }
+        ArrayList<String> testDisplay = new ArrayList<>();
+        for (String ignored : Main.wordArray){
+            testDisplay.add("_");
+        }
+        int i = 0;
+        for (String s : Main.wordArray){
+            if (s.equals("a")){
+                testDisplay.set(i, "a");
+            }
+            i++;
+        }
+        assertEquals(201, Main.letterGuess("a"));
+        assertFalse(Main.interpretGuess(201, "a"));
+        assertEquals(testDisplay, Main.displayedWord);
+        assertTrue(Main.lettersGuessed.contains("a".charAt(0)));
+    }
+    @Test
+    public void guessingCorrectlyShouldReturnWinMessage() {
+        assertEquals(200, Main.letterGuess(Main.word));
+        assertTrue(Main.interpretGuess(200, Main.word));
+    }
 }
